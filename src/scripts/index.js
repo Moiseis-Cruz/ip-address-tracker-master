019138e13dae64133ef44c775c1836cd7e606212
Moiseis-Cruz <moizzito@gmail.com>
2023-06-06 14:34:33 -0300

    Fiz o fecth para trabalhar com API.

---
+++ src/scripts/index.js
@@ -0,0 +1,7 @@
+async function rastreadorDeEndereco() {
+    const response = await fetch("https://geo.ipify.org/api/v2/country?apiKey=at_Ebs0EI2Bfqs4cvI1Yf7yyDhrlHpkH&ipAddress=8.8.8.8")
+    const teste = await response.json()
+    console.log(teste);
+}
+
+rastreadorDeEndereco()
