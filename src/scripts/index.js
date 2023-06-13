e66704f4a6e4254ddfcfee61aa7d38e70f0a3474
Moiseis-Cruz <moizzito@gmail.com>
2023-06-13 09:45:44 -0300

    Busquei os espaços a para apresentar com a busca

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,23 +1,31 @@
+document.querySelector(".btn-search").addEventListener("click", () => {
+    
+})
+
+const addressResult = document.getElementById("address-result")
+const locationResult = document.getElementById("location-result")
+const timezoneResult = document.getElementById("timezone-result")
+const ipsResult = document.getElementById("ips-result")
+
+
 async function addressTracker() {
     const response = await fetch("https://geo.ipify.org/api/v2/country,city?apiKey=at_Ebs0EI2Bfqs4cvI1Yf7yyDhrlHpkH&ipAddress=8.8.8.8")
     const action = await response.json()
     
     const ip = action.ip
-    const location = action.location
+    const timezone = action.location.timezone
     const ips = action.ips
-    console.log(action);
+    const location = action.location
 }
 
 addressTracker()
 
 // async function getAddress() {
-//     console.log(await addressTracker());
-//     console.log(domains);
-// }
+    //     console.log(await addressTracker());
+    //     console.log(domains);
+    // }
+    
+    // getAddress()
+    
 
-// getAddress()
-
-// IP ADDRESS
-// LOCATION
-// TIMEZONE
-// ISP
+    // console.log(location.city, location.region);
