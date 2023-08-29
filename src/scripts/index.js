ba52796559d82f375743cb50805244ec4ea253a2
Moiseis Cruz <moizzito@gmail.com>
2023-08-28 22:23:55 -0300

    Add endereços a chegar ao métodos.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,11 +3,11 @@ const btnSearch = document.getElementById("btn-search")
 btnSearch.addEventListener("click", () => {
     const addressIp = document.getElementById("address-ip").value
 
-
+    
 })
 
 async function address () {
-    // const url = "https://geo.ipify.org/api/v2/country?apiKey=at_GQHZq86hVh3laClN0Rx7smnw5SuKU&ipAddress=8.8.8.8"
+    // const url = "https://geo.ipify.org/api/v2/country,city?apiKey=at_GQHZq86hVh3laClN0Rx7smnw5SuKU&ipAddress=8.8.8.8"
     const response = await fetch(url)
     return await response.json()
 }
@@ -21,8 +21,11 @@ function getAddress() {
 getAddress()
 
 /* 
-IPP ADDRESS
-LOCATION
-TIMEZONE
-ISP
+IPP ADDRESS     {ip}
+LOCATION    {location.city  /   location.region     /   location.geonameId}
+TIMEZONE    {location.timezone}
+ISP     {ips}
+
+LATITUDE    {location.lat}
+LONGITUDE   {location.lng}
 */
